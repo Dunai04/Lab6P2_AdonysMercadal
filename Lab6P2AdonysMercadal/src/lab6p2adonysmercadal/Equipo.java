a3285d87094b6e93fdf3d3aae1b8d650a7ef46d7
Dunai04 <adonysmercadal5@unitec.edu>
2024-02-23 15:28:31 -0600

    4 push clases

---
+++ Lab6P2AdonysMercadal/src/lab6p2adonysmercadal/Equipo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab6p2adonysmercadal;
+
+/**
+ *
+ * @author adony
+ */
+public class Equipo {
+    String equipo;
+    String nombreequipo;
+    String ciudad;
+    String Estadio;
+}
